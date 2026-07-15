7d4fa9998ee95465e13cad7e7f6fbea143ce8400
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-15 12:09:10 +0530

    [3163] [Time Beats: 82.42%] [Memory Beats: 89.80%] - LeetPush

--- 3163-String-Compression-III.cpp
+++ 3163-String-Compression-III.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     string compressedString(string word) {
-        unordered_set<int> st;
         char p='0';
         string comp="";
         int cnt=0;
         for(char c:word){
-            if(p!='0' && st.count(c)==0){
+            if(p!='0' && c!=p){
                 comp.push_back('0'+cnt);
                 comp.push_back(p);
                 cnt=0;
-                st.clear();
             }
-            else if(st.count(c) && cnt==9){
+            else if(c==p && cnt==9){
                 comp.push_back('0'+cnt);
                 comp.push_back(p);
                 cnt=0;
-                st.clear();
             }
             p=c;
             cnt++;
-            st.insert(c);
         }
         if(cnt>0){
                 comp.push_back('0'+cnt);
