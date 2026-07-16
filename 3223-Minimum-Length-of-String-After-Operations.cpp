6186deb43a1dd84dbf8d08cb646f12df37b45250
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-16 10:23:19 +0530

    [3223] [Time Beats: 17.92%] [Memory Beats: 31.48%] - LeetPush

--- 3223-Minimum-Length-of-String-After-Operations.cpp
+++ 3223-Minimum-Length-of-String-After-Operations.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int n=s.length();
-        if(n<3){
-            return n;
-        }
-        unordered_map<int,int> mp1,mp2;
-        for(int i=1;i<n;i++){
-            mp1[s[i]]++;
-        }
-        mp2[s[0]]++;
-        for(int i=1;i<n-1;i++){
-            if(mp1[s[i]]>0) {
-                if(mp2.find(s[i])!=mp2.end() && mp1[s[i]]>1){
-                    mp2[s[i]]--;
-                    mp1[s[i]]--;
-                }
-                mp1[s[i]]--;
-                if(mp1[s[i]]>=0){
-                    mp2[s[i]]++;
-                }
-            }
+        unordered_map<char,int> mp;
+        for(char c:s){
+            mp[c]++;
         }
         int sum=0;
-        for(const auto &[i,v]:mp1){
-            sum+=v;
-        }
-        for(const auto &[i,v]:mp2){
-            sum+=v;
+        for(const auto & [i,v]:mp){
+            if(v<3){
+                sum+=v;
+            }
+            else if(v&1){
+                sum++;
+            }else{
+                sum++;
+                sum++;
+            }
         }
         return sum;
     }
