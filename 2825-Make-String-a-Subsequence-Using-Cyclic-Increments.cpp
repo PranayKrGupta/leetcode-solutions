7578637e6c2e66a40cbc6e206757dc5514df6a34
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-16 11:21:56 +0530

    [2825] [Time Beats: 45.45%] [Memory Beats: 11.50%] - LeetPush

--- 2825-Make-String-a-Subsequence-Using-Cyclic-Increments.cpp
+++ 2825-Make-String-a-Subsequence-Using-Cyclic-Increments.cpp
@@ -5,15 +5,12 @@ public:
         int j=0;
         while(i<str1.length() && j<str2.length()){
             if(str1[i]==str2[j]){
-                i++;
                 j++;
             }else if((char)((str1[i]-'a'+1)%26+'a')==str2[j]){
-                i++;
                 j++;
-            }else{
-                i++;
             }
+                i++;
         }
-    return j==str2.length();
+        return j==str2.length();
     }
 };
