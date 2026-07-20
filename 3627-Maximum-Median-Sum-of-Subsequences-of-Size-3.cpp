336ff54c3b89ffc80f8e38cbfd75c1122a64325c
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-20 10:54:33 +0530

    [3627] [Time Beats: 26.79%] [Memory Beats: 14.03%] - LeetPush

--- 3627-Maximum-Median-Sum-of-Subsequences-of-Size-3.cpp
+++ 3627-Maximum-Median-Sum-of-Subsequences-of-Size-3.cpp
@@ -3,12 +3,11 @@ public:
     long long maximumMedianSum(vector<int>& nums) {
         long long sum=0;
         sort(nums.begin(),nums.end());
-        int i=0;
+        int i=nums.size()/3;
         int j=nums.size()-1;
         while(i<j){
-            sum+=nums[j-1];
-            i++;
-            j-=2;
+            sum+=nums[i];
+            i+=2;
         }
         return sum;
     }
