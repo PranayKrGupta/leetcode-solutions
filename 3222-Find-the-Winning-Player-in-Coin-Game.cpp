fb8b8bebbeefb2d5736310e2e8ac7a0c72153b3d
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-20 11:15:21 +0530

    [3222] [Time Beats: 100.00%] [Memory Beats: 63.76%] - LeetPush

--- 3222-Find-the-Winning-Player-in-Coin-Game.cpp
+++ 3222-Find-the-Winning-Player-in-Coin-Game.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     string winningPlayer(int x, int y) {
-        bool f=0;
-        while(x>=1 && y>=4){
-            x--;
-            y-=4;
-            f=!f;
-        }
-        if(f){
-            return "Alice";
-        }
+        int limiting_variable = min(x,y/4);
+        if(limiting_variable&1) return "Alice";
         return "Bob";
     }
 };
