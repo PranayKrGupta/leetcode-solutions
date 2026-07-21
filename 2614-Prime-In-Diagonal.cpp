f7bae294af7d9ab9a94363720f6259223ec12f54
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-21 10:35:53 +0530

    [2614] [Time Beats: 41.82%] [Memory Beats: 70.70%] - LeetPush

--- 2614-Prime-In-Diagonal.cpp
+++ 2614-Prime-In-Diagonal.cpp
@@ -1,26 +1,20 @@
 class Solution {
-    void sieve(vector<bool> &arr){
-        arr[0]=false;
-        arr[1]=false;
-        for(int i=2;i*i<arr.size();i++){
-            if(arr[i]){
-                for(int p=i*i;p<arr.size();p+=i){
-                    arr[p]=false;
-                }
-            }
+    bool isPrime(int n){
+        if(n<2) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
         }
+        return true;
     }
 public:
     int diagonalPrime(vector<vector<int>>& nums) {
-        vector<bool> isPrime(4*1e6+1,true);
-        sieve(isPrime);
         int n=nums.size();
         int m=nums[0].size();
         int i=0;
         int j=0;
         int maxi=0;
         while(i<n && j<m){
-            if(isPrime[nums[i][j]]){
+            if(isPrime(nums[i][j])){
                 maxi=max(nums[i][j],maxi);
             }
             i++;
@@ -29,7 +23,7 @@ public:
         i=0;
         j=m-1;
         while(i<n && j>=0){
-            if(isPrime[nums[i][j]]){
+            if(isPrime(nums[i][j])){
                 maxi=max(nums[i][j],maxi);
             }
             i++;
