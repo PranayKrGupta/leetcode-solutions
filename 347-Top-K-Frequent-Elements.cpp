36b78bd47c904be3e6efbf24ce2d216de810974b
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-22 10:08:33 +0530

    [347] [Time Beats: 57.93%] [Memory Beats: 62.95%] - LeetPush

--- 347-Top-K-Frequent-Elements.cpp
+++ 347-Top-K-Frequent-Elements.cpp
@@ -1,7 +1,12 @@
 class Solution {
+    struct com{
+        bool operator()(const auto &a,const auto &b) const{
+            return a.first>b.first;
+        }
+    };
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> q;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,com> q;
         unordered_map<int,int> mp;
         for(int i:nums){
             mp[i]++;
@@ -9,9 +14,12 @@ public:
         for(const auto &[i,v]:mp){
             pair<int,int> p={v,i};
             q.push(p);
+            if(q.size()>k){
+                q.pop();
+            }
         }
         vector<int> ans;
-        for(int i=0;i<k;i++){
+        while(!q.empty()){
             ans.push_back(q.top().second);
             q.pop();
         }
