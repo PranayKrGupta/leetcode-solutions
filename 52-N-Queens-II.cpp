93c6fe1c0e08983be3a2f0d8f1df16fa1460ddd0
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-22 14:36:06 +0530

    [52] [Time Beats: 54.33%] [Memory Beats: 80.32%] - LeetPush

--- 52-N-Queens-II.cpp
+++ 52-N-Queens-II.cpp
@@ -1,64 +1,18 @@
 class Solution {
-    bool check(vector<string>& mat, int r, int c) {
-        int n = mat.size();
-        for (int i = 0; i < n; i++) {
-            if (mat[r][i] == 'Q')
-                return false;
-        }
-        for (int i = 0; i < n; i++) {
-            if (mat[i][c] == 'Q')
-                return false;
-        }
-        int r2 = r;
-        int c2 = c;
-        while (r2 >= 0 && c2 >= 0) {
-            if (mat[r2][c2] == 'Q')
-                return false;
-            r2--;
-            c2--;
-        }
-
-        r2 = r;
-        c2 = c;
-
-        while (r2 < n && c2 < n) {
-            if (mat[r2][c2] == 'Q')
-                return false;
-            r2++;
-            c2++;
-        }
-        r2 = r;
-        c2 = c;
-
-        while (r2 >= 0 && c2 < n) {
-            if (mat[r2][c2] == 'Q')
-                return false;
-            r2--;
-            c2++;
-        }
-        r2 = r;
-        c2 = c;
-
-        while (r2 < n && c2 >= 0) {
-            if (mat[r2][c2] == 'Q')
-                return false;
-            r2++;
-            c2--;
-        }
-
-        return true;
-    }
-    int nqueens(vector<string>& mat, int n, int r) {
-        if (n == 0) {
+    int nqueens(int size, int r,vector<bool> &uvc,vector<bool> &uvpd,vector<bool> &uvnd) {
+        if (r==size) {
             return 1;
         }
-        int size = mat.size();
         int c = 0;
         for (int i = 0; i < size; i++) {
-            if (check(mat, r, i)) {
-                mat[r][i] = 'Q';
-                c += nqueens(mat, n - 1, r + 1);
-                mat[r][i] = '.';
+            if (uvc[i] &&uvnd[r-i+(size-1)] &&uvpd[r+i]) {
+                uvc[i]=0;
+                uvnd[r-i+(size-1)]=0;
+                uvpd[r+i]=0;
+                c += nqueens(size, r + 1,uvc,uvpd,uvnd);
+                uvc[i]=1;
+                uvnd[r-i+(size-1)]=1;
+                uvpd[r+i]=1;
             }
         }
         return c;
@@ -66,8 +20,9 @@ class Solution {
 
 public:
     int totalNQueens(int n) {
-        vector<string> mat(n, string(n, '.'));
-
-        return nqueens(mat, n, 0);
+        vector<bool> uvc(n,true);
+        vector<bool> uvpd(2*n,true);
+        vector<bool> uvnd(2*n,true);
+        return nqueens(n,0,uvc,uvpd,uvnd);
     }
 };
