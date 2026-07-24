68d17aaf974b33fc7f235afb03d300667d0ff5d7
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-24 14:28:26 +0530

    [111] [Time Beats: 100.00%] [Memory Beats: 52.13%] - LeetPush

--- 111-Minimum-Depth-of-Binary-Tree.cpp
+++ 111-Minimum-Depth-of-Binary-Tree.cpp
@@ -6,21 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x),
+ * left(left),right(right) {}
  * };
  */
 class Solution {
 public:
     int minDepth(TreeNode* r) {
-        if(r==nullptr) return 0;
-        int lh= minDepth(r->left);
-        int rh=minDepth(r->right);
-        if(lh>0 && rh>0)
-        return min(lh,rh)+1;
-        if(lh==0)
-        return rh+1;
-        if(rh==0)
-        return lh+1;
-        return 1;
+        if (r == nullptr)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(r);
+        int l = 1;
+        while (q.size()) {
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
+                TreeNode* n = q.front();
+                q.pop();
+                if (n->left == nullptr && n->right == nullptr) {
+                    return l;
+                }
+                if (n->left != nullptr) {
+                    q.push(n->left);
+                }
+                if (n->right != nullptr) {
+                    q.push(n->right);
+                }
+            }
+            l++;
+        }
+        return l;
     }
 };
