20383702b0d837fc69e26256412f927d8b28885c
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-27 13:13:42 +0530

    [1464] [Time Beats: 100.00%] [Memory Beats: 99.05%] - LeetPush

--- 1464-Maximum-Product-of-Two-Elements-in-an-Array.cpp
+++ 1464-Maximum-Product-of-Two-Elements-in-an-Array.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxPro=INT_MIN;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j){
-                    maxPro=max((nums[i]-1)*(nums[j]-1),maxPro);
-                }
+        int max1=INT_MIN;
+        int max2=INT_MIN;
+        for(int i:nums){
+            if(max1<i){
+                max2=max1;
+                max1=i;
+            }else if( i>max2){
+                max2=i;
             }
         }
-        return maxPro;
+        if(max2==INT_MIN){
+            max2=max1;
+        }
+        return (max1-1)*(max2-1);
     }
 };
