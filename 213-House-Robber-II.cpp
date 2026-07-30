49f5b6843aa7e27fe1861f5bcfe6dfae0c4be42e
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-30 14:22:14 +0530

    [213] [Time Beats: 100.00%] [Memory Beats: 19.19%] - LeetPush

---
+++ 213-House-Robber-II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int solve(int i,int n,vector<int> &nums,vector<int> &dp){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int pick=nums[i]+solve(i+2,n,nums,dp);
+        int notPick = 0+solve(i+1,n,nums,dp);
+        return dp[i]=max(pick,notPick);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(n+1,-1);
+        vector<int> dp2(n+1,-1);
+        return max(solve(0,n-1,nums,dp1),solve(1,n,nums,dp2));
+    }
+};
