933f11ee2437551fb27d529ba1a9c56a731e43ad
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-08-01 11:26:59 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 30.85%] - LeetPush

--- 62-Unique-Paths.cpp
+++ 62-Unique-Paths.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    int solve(vector<vector<int>> &dp,int m,int n,int i,int j){
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==m-1){
-            return dp[i][j]=solve(dp,m,n,i,j+1);
-        }else if(j==n-1){
-            return dp[i][j]=solve(dp,m,n,i+1,j);
-        }else{
-            return dp[i][j]=solve(dp,m,n,i,j+1)+solve(dp,m,n,i+1,j);
-        }
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
-        return solve(dp,m,n,0,0);
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) continue;
+                else if(j==n-1){
+                    dp[i][j]=dp[i+1][j];
+                }else if(i==m-1){
+                    dp[i][j]=dp[i][j+1];
+                }else{
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
