bcf27f1822e61583d7cd40e7916b4b46b7855f8f
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-08-01 10:38:46 +0530

    [1137] [Time Beats: 100.00%] [Memory Beats: 69.46%] - LeetPush

--- 1137-N-th-Tribonacci-Number.cpp
+++ 1137-N-th-Tribonacci-Number.cpp
@@ -1,13 +1,18 @@
-class Solution {
-    int solve(vector<int> &dp,int n){
-        if(n<=1) return n;
-        if(n==2) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2)+solve(dp,n-3);
-    }   
+class Solution{  
 public:
     int tribonacci(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(dp,n);
+        if(n<=1) return n;
+        if(n==2) return 1;
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=0;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
     }
 };
