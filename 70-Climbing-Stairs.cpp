a04bd72c573b04439949187821ab989cec8f50b9
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-08-01 10:18:10 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 74.41%] - LeetPush

--- 70-Climbing-Stairs.cpp
+++ 70-Climbing-Stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+        if(n<=1) return n;
+        int a=1;
+        int b=1;
+        int c=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return dp[n];
+        return b;
     }
 };
