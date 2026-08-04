7e1cb82a46c6f9e819ac0f52acdc167b05077dc9
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-08-04 13:55:26 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 90.19%] - LeetPush

--- 198-House-Robber.cpp
+++ 198-House-Robber.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    int solve(int i,vector<int> &v,vector<int> &dp){
-        int n=v.size();
-        if(i>=n){return 0;}
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int pick=v[i]+solve(i+2,v,dp);
-        int notPick = 0+solve(i+1,v,dp);
-        return dp[i]=max(pick,notPick);
-    }
 public:
-    int rob(vector<int>& v) {
-        int n=v.size();
-        vector<int> dp(n+1,-1);
-        solve(0,v,dp);
-        return dp[0];
+    int rob(vector<int>& nums) {
+        int a=0;
+        int b=0;
+        int c=0;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            c=max(nums[i]+a,b);
+            a=b;
+            b=c;
+        }
+        return c;
     }
 };
