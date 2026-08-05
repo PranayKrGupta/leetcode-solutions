bfaa8bffeac18d3b666e842d34eb051b35e76e2f
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-08-06 00:06:20 +0530

    [3731] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- 3731-Find-Missing-Elements.cpp
+++ 3731-Find-Missing-Elements.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int maxi=INT_MIN;
         int mini=INT_MAX;
-        unordered_set<int> st;
+        vector<int> arr(101);
         for(int i:nums){
             maxi=max(maxi,i);
             mini=min(mini,i);
-            st.insert(i);
+            arr[i]=1;
         }
         vector<int> ans;
         for(int i=mini;i<=maxi;i++){
-            if(!st.count(i)) ans.push_back(i);
+            if(!arr[i]) ans.push_back(i);
         }
         return ans;
     }
